e2cbabc2c5b53f6beca90367d29d5b2352123400
clairemcguire <123483507+clairemcguire24@users.noreply.github.com>
2023-01-24 15:41:17 +0000

    added a comment

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,9 @@ import java.awt.*;
 import javax.swing.*;
 
 public class Main {
+  
     public static void main (String [] args){
+      System.out.println("made a comment in replit");
 
         JFrame myFrame = new JFrame();
         myFrame.setSize(500,500);
